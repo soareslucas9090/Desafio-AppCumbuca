b568b0f70f54e05546874e6dc90dc60c9f6280f6
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-19 14:08:51 -0300

    test fix: A função de leitura não estava passando corretamente o input

--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -3,13 +3,11 @@ defmodule DesafioCliTest do
   import ExUnit.CaptureIO
   alias DesafioCli
 
-  test "leitura sem registro" do
+  test "Leitura sem registro" do
     input = "0\n"
-    output = capture_io(fn ->
-      send self(), {:io_request, self(), {self(), :put_chars, :unicode, input}}
+    output = capture_io(input, fn ->
       DesafioCli.main()
     end)
-    IO.inspect(output)
     assert output == """
            ###   Ferramenta de numeração de reis e rainhas de Cumbuquia   ###
            Insira os nomes desajados um por linha. Envie "0" para fianlizar.
