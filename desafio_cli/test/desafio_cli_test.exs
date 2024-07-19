49fe46a7968f57a539e76ff22f325a894c39a6ae
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-19 13:32:36 -0300

    test: leitura sem registro

--- desafio_cli/test/desafio_cli_test.exs
+++ desafio_cli/test/desafio_cli_test.exs
@@ -1,8 +1,19 @@
 defmodule DesafioCliTest do
   use ExUnit.Case
-  doctest DesafioCli
+  import ExUnit.CaptureIO
+  alias DesafioCli
 
-  test "greets the world" do
-    assert DesafioCli.hello() == :world
+  test "leitura sem registro" do
+    input = "0\n"
+    output = capture_io(fn ->
+      send self(), {:io_request, self(), {self(), :put_chars, :unicode, input}}
+      DesafioCli.main()
+    end)
+    IO.inspect(output)
+    assert output == """
+           ###   Ferramenta de numeração de reis e rainhas de Cumbuquia   ###
+           Insira os nomes desajados um por linha. Envie "0" para fianlizar.
+           ###   Nenhum nome enviado!   ###
+           """
   end
 end
