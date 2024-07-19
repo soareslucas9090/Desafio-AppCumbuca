d2f9a77a73d653640d1ec5c5190d78fb1726a143
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-19 14:28:50 -0300

    test: inserção fora de ordem

--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -31,4 +31,21 @@ defmodule DesafioCliTest do
           Maria I
           """
   end
+
+  test "Inserção fora de ordem" do
+    input = "Maria\nAna\nAna\nJoão\nAna\nJoão\n0\n"
+    output = capture_io(input, fn ->
+      DesafioCli.main()
+    end)
+    assert output == """
+          ###   Ferramenta de numeração de reis e rainhas de Cumbuquia   ###
+          Insira os nomes desajados um por linha. Envie "0" para fianlizar.
+          Ana I
+          Ana II
+          Ana III
+          João I
+          João II
+          Maria I
+          """
+  end
 end
