db3e7157c8d07dd3bf97110412202f05b1fa0b1f
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-19 01:32:19 -0300

    feat: leitura de lista de nomes

--- desafio_cli/lib/desafio_cli.ex
+++ desafio_cli/lib/desafio_cli.ex
@@ -16,6 +16,16 @@ defmodule DesafioCli do
     IO.puts("###   Ferramenta de numeração de reis e rainhas de Cumbuquia   ###")
     string1 = ~s(Insira os nomes desajados um por linha. Envie "0" para fianlizar.)
     IO.puts(string1)
+    leitura_de_nomes()
   end
 
+  defp leitura_de_nomes(acc \\ []) do
+    input = IO.gets("") |> String.trim()
+
+    if input == "0" do
+      acc
+    else
+      leitura_de_nomes(acc ++ [input])
+    end
+  end
 end
